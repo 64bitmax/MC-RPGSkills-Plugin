cba7838658cb21f1606f29236284aa167e3945d3
maxno <maxnorman14@googlemail.com>
2019-05-10 03:52:00 +0100

    Initial commit

---
+++ src/main/java/com/devnorman/rpgskills/RPGSkills.java
@@ -0,0 +1,17 @@
+package com.devnorman.rpgskills;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPGSkills extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
